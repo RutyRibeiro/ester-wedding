7a38a7770f66ffaf2e4bd29b306e5500654338a7
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2022-06-27 22:46:10 -0300

    Configurando banco de dados e tabela de produtos

---
+++ src/models/products.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+const database = require ("../db");
+
+const Products = database.define('products', {
+    product_id:{
+        type: DataTypes.STRING,
+        allowNull: true,
+        primaryKey: true,
+    },
+    product_name:{
+        type: DataTypes.STRING,
+    },   
+    product_desc:{
+        type: DataTypes.STRING,
+    },
+    product_img:{
+        type: DataTypes.STRING,
+    },  
+    product_reserved:{
+        type: DataTypes.BOOLEAN,
+    },
+    client_name:{
+        type: DataTypes.STRING,
+    },
+    client_email:{
+        type: DataTypes.STRING,
+    },
+
+});
+
+Products.sync();
+
+module.exports = Products;
